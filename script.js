2ccce659db0106a46d78fd992c32dfd07f432a1c
venkatesh <burravenkatesh284@gmail.com>
2024-10-22 18:48:10 +0530

    Updated image paths and fixed issues

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const menuIcon = document.querySelector(".menu");
 menuIcon.addEventListener("click", () => {
   expand.classList.toggle("listContent");
   if (expand.classList.contains("listContent")) {
-    menuIcon.src = "/images/delete.png";
+    menuIcon.src = "images/delete.png";
   } else {
-    menuIcon.src = "/images/menu.png";
+    menuIcon.src = "images/menu.png";
   }
 });
